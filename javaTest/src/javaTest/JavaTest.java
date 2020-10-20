d929fafd896e02750851233d8b0006db79466281
whaojun <diansuan02@wenghj>
2020-10-20 14:09:43 +0800

    local com1

--- javaTest/src/javaTest/JavaTest.java
+++ javaTest/src/javaTest/JavaTest.java
@@ -4,5 +4,6 @@ public class JavaTest {
   public static void main(String args[]) {
 	  System.err.println("ver2.2");
 	  System.err.println("ver2.2git");
+	  System.err.println("local com1");
   }
 }

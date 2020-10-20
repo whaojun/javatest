d35bf35180849f29a08156ea9bdcc62b32c7d316
whaojun <diansuan02@wenghj>
2020-10-20 14:39:55 +0800

    1

--- javaTest/src/javaTest/JavaTest.java
+++ javaTest/src/javaTest/JavaTest.java
@@ -2,7 +2,9 @@ package javaTest;
 
 public class JavaTest {
   public static void main(String args[]) {
-	  System.err.println("ver2.2");
-	  System.err.println("ver2.2git");
+
+	  System.err.println(1212344);
+	  System.err.println(1212355);
+
   }
 }

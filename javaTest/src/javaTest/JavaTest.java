c9a3de7bd704f774bcb25406f06aea0ec098e110
whaojun <>
2020-10-20 14:15:14 +0800

    Revert "local com3"
    This reverts commit e9db49276b4d0b3cb39585dc893abb46aa5374cc.

--- javaTest/src/javaTest/JavaTest.java
+++ javaTest/src/javaTest/JavaTest.java
@@ -6,7 +6,5 @@ public class JavaTest {
 	  System.err.println("ver2.2git");
 	  System.err.println("local com1");
 	  System.err.println("local com2");
-	  System.err.println("local com3");
-
   }
 }
